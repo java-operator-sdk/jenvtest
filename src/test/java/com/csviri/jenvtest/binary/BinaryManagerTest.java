768bbf62e976bd64bcd6c886fe5c942fd7bd476f
csviri <csviri@gmail.com>
2023-03-13 10:41:22 +0100

    tests

--- src/test/java/com/csviri/jenvtest/binary/BinaryManagerTest.java
+++ src/test/java/com/csviri/jenvtest/binary/BinaryManagerTest.java
@@ -1,5 +1,8 @@
 package com.csviri.jenvtest.binary;
 
+import com.csviri.jenvtest.APIServerConfig;
+import com.csviri.jenvtest.APIServerConfigBuilder;
+import com.csviri.jenvtest.JenvtestException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +11,12 @@ class BinaryManagerTest {
 
     @Test
     void throwsExceptionIfBinaryNotPresentAndInOfflineMode() {
+        BinaryManager binaryManager = new BinaryManager(APIServerConfigBuilder.anAPIServerConfig()
+                .withDownloadBinaries(false)
+                .withApiServerVersion("1.0.1")
+                .build());
 
+        assertThrows(JenvtestException.class, binaryManager::initAndDownloadIfRequired);
     }
 
 }
