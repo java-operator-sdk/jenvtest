391bb00e641487af98fcea5df67986df20e219bc
Chris Laprun <metacosm@users.noreply.github.com>
2023-03-26 17:19:19 +0200

    feat: ARM-arch is now supported, avoid always recomputing values (#48)

--- core/src/main/java/io/javaoperatorsdk/jenvtest/binary/OSInfo.java
+++ core/src/main/java/io/javaoperatorsdk/jenvtest/binary/OSInfo.java
@@ -1,30 +1,37 @@
 package io.javaoperatorsdk.jenvtest.binary;
 
 public class OSInfo {
+  private final String os;
+  private final String arch;
+
+  public OSInfo() {
+    final var osArch = System.getProperty("os.arch").toLowerCase();
+    switch (osArch) {
+      case "ppc64":
+        arch = "ppc64le";
+        break;
+      case "aarch64":
+        arch = "arm64";
+        break;
+      default:
+        arch = "amd64";
+    }
+
+    final var os = System.getProperty("os.name").toLowerCase();
+    if (os.contains("win")) {
+      this.os = "windows";
+    } else if (os.contains("mac")) {
+      this.os = "darwin";
+    } else {
+      this.os = os;
+    }
+  }
 
   public String getOSName() {
-    String os = System.getProperty("os.name").toLowerCase();
-    if (os.contains("win")) {
-      return "windows";
-    } else if (os.contains("mac")) {
-      return "darwin";
-    } else {
-      return os;
-    }
+    return os;
   }
 
-  /**
-   * Only amd64 and ppc64 binaries available.
-   *
-   * @return ppc64le if on that architecture otherwise amd64.
-   */
   public String getOSArch() {
-    var osArch = System.getProperty("os.arch").toLowerCase();
-    if (osArch.contains("ppc64")) {
-      return "ppc64le";
-    } else {
-      return "amd64";
-    }
+    return arch;
   }
-
 }
