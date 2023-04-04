a87ae49089365d61933337a6f367af646101a80e
csviri <csviri@gmail.com>
2023-04-04 13:09:11 +0200

    revert to check windows problem

--- core/src/main/java/io/javaoperatorsdk/jenvtest/kubeconfig/KubeConfig.java
+++ core/src/main/java/io/javaoperatorsdk/jenvtest/kubeconfig/KubeConfig.java
@@ -2,7 +2,6 @@ package io.javaoperatorsdk.jenvtest.kubeconfig;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -32,7 +31,7 @@ public class KubeConfig {
 
   public void updateKubeConfig(int apiServerPort) {
     log.debug("Updating kubeconfig");
-    previousCurrentContext = execWithKubectlConfigAndWait("current-context").trim();
+    // previousCurrentContext = execWithKubectlConfigAndWait("current-context").trim();
     execWithKubectlConfigAndWait("set-cluster", JENVTEST,
         "--server=https://127.0.0.1:" + apiServerPort,
         "--certificate-authority=" + certManager.getAPIServerCertPath());
@@ -50,9 +49,9 @@ public class KubeConfig {
     unset("clusters." + JENVTEST);
     unset("users." + JENVTEST);
     unset("current-context");
-    if (previousCurrentContext != null && !previousCurrentContext.isEmpty()) {
-      execWithKubectlConfigAndWait("use-context", previousCurrentContext);
-    }
+    // if (previousCurrentContext != null && !previousCurrentContext.isEmpty()) {
+    // execWithKubectlConfigAndWait("use-context", previousCurrentContext);
+    // }
   }
 
   private void unset(String target) {
@@ -78,12 +77,13 @@ public class KubeConfig {
       args.add("config");
       args.addAll(List.of(arguments));
       var process = new ProcessBuilder(args).start();
-      String stdout;
-      try (InputStream is = process.getInputStream()) {
-        stdout = IOUtils.toString(is, Charset.defaultCharset());
-      }
+      // String stdout;
+      // try (InputStream is = process.getInputStream()) {
+      // stdout = IOUtils.toString(is, Charset.defaultCharset());
+      // }
       process.waitFor();
-      return stdout;
+      return null;
+      // return stdout;
     } catch (IOException e) {
       throw new JenvtestException(e);
     } catch (InterruptedException e) {
