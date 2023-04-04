94d506ee29f860403174c3ed0dad9a5940bea070
csviri <csviri@gmail.com>
2023-04-04 13:43:13 +0200

    wip

--- core/src/main/java/io/javaoperatorsdk/jenvtest/KubeAPIServer.java
+++ core/src/main/java/io/javaoperatorsdk/jenvtest/KubeAPIServer.java
@@ -55,9 +55,7 @@ public class KubeAPIServer implements UnexpectedProcessStopHandler {
     log.debug("Stopping");
     kubeApiServerProcess.stopApiServer();
     etcdProcess.stopEtcd();
-    // if (config.isUpdateKubeConfig()) {
     kubeConfig.restoreKubeConfig();
-    // }
     etcdProcess.cleanEtcdData();
     log.debug("Stopped");
   }
