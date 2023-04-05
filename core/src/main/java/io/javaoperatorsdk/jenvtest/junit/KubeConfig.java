1a83dd9f55ab2baea8b4207d59c1c2dbe4894975
csviri <csviri@gmail.com>
2023-04-05 16:04:15 +0200

    docs

--- core/src/main/java/io/javaoperatorsdk/jenvtest/junit/KubeConfig.java
+++ core/src/main/java/io/javaoperatorsdk/jenvtest/junit/KubeConfig.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Used to annotate String field in JUnit test to inject Kube Config yaml.
+ */
 @Target({FIELD})
 @Retention(RUNTIME)
 public @interface KubeConfig {
