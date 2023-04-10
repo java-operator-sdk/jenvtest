3374f3e3d1d1c692cdbe1ebedbceb9e1c858f969
csviri <csviri@gmail.com>
2023-04-10 15:59:51 +0200

    fix: env var naming

--- core/src/main/java/io/javaoperatorsdk/jenvtest/KubeAPIServerConfigBuilder.java
+++ core/src/main/java/io/javaoperatorsdk/jenvtest/KubeAPIServerConfigBuilder.java
@@ -67,7 +67,7 @@ public final class KubeAPIServerConfigBuilder {
       }
     }
     if (waitForEtcdHealthCheckOnStartup == null) {
-      var waitForEtcdHealthCheckOnStartup = System.getenv(JENVTEST_API_SERVER_VERSION_ENV_VAR);
+      var waitForEtcdHealthCheckOnStartup = System.getenv(JENVTEST_WAIT_FOR_ETCD_HEALTH_CHECK);
       if (waitForEtcdHealthCheckOnStartup != null) {
         this.waitForEtcdHealthCheckOnStartup =
             Boolean.parseBoolean(waitForEtcdHealthCheckOnStartup);
